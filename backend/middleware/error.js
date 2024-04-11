d6f52c084a692a7ba0eda6f3c0b38b205c2aa5f7
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-04-11 13:21:26 +0530

    handle the ErrorHandler

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,38 +1,35 @@
 const ErrorHander = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
-  
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
 
-  //Wrong Mongodb Id error
+  // Wrong Mongodb Id error
   if (err.name === "CastError") {
     const message = `Resource not found. Invalid: ${err.path}`;
-    const customError = new ErrorHandler(message, 400);
-    return next(customError); // Pass the custom error to the error handling middleware
+    err = new ErrorHander(message, 400);
   }
 
-  //Mongoose duplicate key error
-  if(err.code = 11000){
-    const message = `Duplicate ${Object.keys(err.keyValue)} ENtered`;
-    err=new ErrorHander(message,400);
-  }
- 
-  //Wrong JWT error
-  if(err.code = "jsonWebTokenError"){
-    const message = `Json Web TOken is invalid, try again`;
-    err=new ErrorHander(message,400);
-  }
-  
-  //JWT Expire Error
-  if(err.code ="TokenExpiredError"){
-    const message = `Json Web TOken is Expired, try again`;
-    err=new ErrorHander(message,400);
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHander(message, 400);
   }
 
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again `;
+    err = new ErrorHander(message, 400);
+  }
 
-  res.status(statusCode).json({
+  // JWT EXPIRE error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is Expired, Try again `;
+    err = new ErrorHander(message, 400);
+  }
+
+  res.status(err.statusCode).json({
     success: false,
-    message: message,
+    message: err.message,
   });
 };
